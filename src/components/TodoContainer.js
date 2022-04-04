eabdd07725c635e93cdf769732f068faac6ef54d
franklin <franklinbenjamin032@gmail.com>
2022-04-03 22:01:05 -0400

    add list code

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -54,3 +54,37 @@ class TodoContainer extends React.Component {
   }
 }
 export default TodoContainer;
+
+class InputTodo extends Component {
+  state = {
+    title: '',
+  };
+
+  addTodoName = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add Todo..." value={this.state.title} name="title" onChange={this.addTodoName} />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
